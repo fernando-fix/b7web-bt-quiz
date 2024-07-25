44899af6a85acfa5b12b867294fd4fdf45534229
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2024-07-24 22:17:01 -0300

    script.js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let body = document.querySelector('body');
+
+// Carregar o tema
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('tema')) {
+        if (localStorage.getItem('tema') === 'dark') {
+            body.classList.add('theme-dark');
+            body.classList.remove('theme-light');
+        } else {
+            body.classList.remove('theme-dark');
+            body.classList.add('theme-light');
+        }
+    } else {
+        localStorage.setItem('tema', 'light');
+        body.classList.add('theme-light');
+    }
+});
+
+function trocarTema() {
+    body.classList.toggle('theme-dark');
+    if (body.classList.contains('theme-dark')) {
+        localStorage.setItem('tema', 'dark');
+    } else {
+        localStorage.setItem('tema', 'light');
+    }
+}
